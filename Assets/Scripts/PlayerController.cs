f1147111fd2480d4b334d9dde6e0cf6e73cc351a
tdamron <TristanDamron31@gmail.com>
2019-01-25 22:23:46 -0800

    Added player controller and player prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed_;
+    [SerializeField]
+    private bool jumping_;
+    private Rigidbody rb_;
+
+    void Start()
+    {
+        rb_ = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var pos = new Vector3();
+        pos.x = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed_;   
+        pos.y = transform.position.y;
+
+        if (!jumping_ && Input.GetAxis("Jump") != 0f) {
+            Jump(5f);
+        }        
+
+        pos.z = transform.position.z;
+        transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision c) {
+        if (c.gameObject.layer == LayerMask.NameToLayer("World")) {
+            jumping_ = false;
+        }
+    }
+
+    public void Jump(float thrust_) {
+        rb_.AddForce(transform.up * thrust_, ForceMode.Impulse);   
+        jumping_ = true;
+    }
+}
