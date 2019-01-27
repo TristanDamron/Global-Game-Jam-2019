c5bec38ed400e41e11c226628708202f33c4f229
Matt O'Tousa <mlotousa@gmail.com>
2019-01-26 19:48:40 -0800

    updated player controller collision checks (in progress)

--- Assets/Scripts/MO_PlayerController.cs
+++ Assets/Scripts/MO_PlayerController.cs
@@ -8,7 +8,9 @@ public class MO_PlayerController : MonoBehaviour
     public float yVelocityUpperLimit_ = 20.0f;
     public float extraJumpGravity_ = 12.0f;
     [SerializeField]
-    private float speed_;
+    private float walkSpeed_;
+    [SerializeField]
+    private float airSpeed_;
     [SerializeField]
     private float jumpForce_ = 600.0f;
     [SerializeField]
@@ -21,6 +23,11 @@ public class MO_PlayerController : MonoBehaviour
     private Animator animator_;
     [SerializeField]
     private PlayerState playerState = PlayerState.AIRBORNE;
+
+    private Vector3 top_;
+    private Vector3 right_;
+    private Vector3 bottom_;
+    private Vector3 left_;
 
     public enum PlayerState
     {
@@ -35,10 +42,25 @@ public class MO_PlayerController : MonoBehaviour
         rb_ = GetComponent<Rigidbody2D>();
         particles_ = GetComponent<ParticleSystem>();
         animator_ = GetComponentInChildren<Animator>();
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        Vector3 colliderCenter = collider.bounds.center;
+        Vector3 colliderMin = collider.bounds.min;
+        Vector3 colliderMax = collider.bounds.min;
+        bottom_ = new Vector3(colliderCenter.x, colliderMin.y, colliderCenter.z);
+        top_ = new Vector3(colliderCenter.x, colliderMax.y, colliderCenter.z);
+        right_ = new Vector3(colliderMax.x, colliderCenter.y, colliderCenter.z);
+        left_ = new Vector3(colliderMin.x, colliderCenter.y, colliderCenter.z);
     }
 
     void Update()
     {
+        Vector3 moveInput = Vector3.zero;
+
+        if (playerState != PlayerState.YOYO && IsAirborne())
+        {
+            playerState = PlayerState.AIRBORNE;
+        }
+
         if (playerState == PlayerState.AIRBORNE)
         {
             if (rb_.velocity.y == 0)
@@ -75,7 +97,7 @@ public class MO_PlayerController : MonoBehaviour
                 rb_.velocity = new Vector2(rb_.velocity.x, yVelocityUpperLimit_);
 
             Vector3 velocity = rb_.velocity;
-            velocity.x = horizontalInput * speed_;
+            velocity.x = horizontalInput * walkSpeed_;
             // transform.position = pos;
             rb_.velocity = velocity;
 
@@ -129,6 +151,14 @@ public class MO_PlayerController : MonoBehaviour
         rb_.velocity = velocity;
 
         playerState = PlayerState.AIRBORNE;
+    }
+
+    public bool IsAirborne()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(bottom_, Vector3.down, 0.2f, 1 << LayerMask.NameToLayer("World"));
+        if (hit && hit.collider)
+            return false;
+        return true;
     }
 
     public void QuitYoyo()
