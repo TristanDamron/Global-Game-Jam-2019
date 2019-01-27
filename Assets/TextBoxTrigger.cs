23c64fde126f511343fa2d1b647052fcbc1b9fdb
Tattomoosa <mlotousa@gmail.com>
2019-01-27 14:08:06 -0800

    TextBox stuff

---
+++ Assets/TextBoxTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxTrigger : MonoBehaviour
+{
+    public string[] textboxesContents;
+    public bool hideOnStart = false;
+
+    void Start()
+    {
+        if (hideOnStart)
+            GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (!c.GetComponent<PlayerController>()) return;
+        TextBoxController.CreateTextBoxes(textboxesContents);
+        Destroy(this.gameObject);
+    }
+}
