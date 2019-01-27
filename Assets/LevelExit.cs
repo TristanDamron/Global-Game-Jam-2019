f5e0abe0bd45402f0dda8890a7de46516314327b
Tattomoosa <mlotousa@gmail.com>
2019-01-27 12:54:40 -0800

    level exit added

---
+++ Assets/LevelExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    public string nextLevelName;
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.GetComponent<PlayerController>())
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
+    }
+}
