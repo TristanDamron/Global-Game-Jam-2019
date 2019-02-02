60228e4291dc01fb542337b7c6cab64b8bcbcc16
Tattomoosa <mlotousa@gmail.com>
2019-02-01 20:07:15 -0800

    overlay bugfix

--- Assets/Scripts/Overlay.cs
+++ Assets/Scripts/Overlay.cs
@@ -5,34 +5,33 @@ using UnityEngine.UI;
 
 public class Overlay : MonoBehaviour
 {
+    static public Overlay activeOverlay;
 
-    // public string overlayText;
-
-    static public bool isActive;
-
-    private Text text;
+    private bool isInitialized = false;
 
     private void Start()
     {
         gameObject.SetActive(false);
+        isInitialized = true;
     }
 
     private void OnEnable()
     {
-        if (isActive)
+        if (!isInitialized) return;
+
+        if (activeOverlay)
         {
-            Debug.LogError("TWO INSTANCES OF OVERLAY CONTROLLER ACTIVE!");
-            gameObject.SetActive(false);
+            Debug.LogError("TWO INSTANCES OF OVERLAY CONTROLLER ACTIVE!\n" +
+                           "Active Overlay: " + activeOverlay.name + "\n" +
+                           "New Overlay: " + this.name);
+            // gameObject.SetActive(false);
+            // return;
+            activeOverlay.gameObject.SetActive(false);
         }
-        isActive = true;
-        text = GetComponentInChildren<Text>();
-        /*
-        if (text)
-            text.text = overlayText;
-            */
+        activeOverlay = this;
     }
     private void OnDisable()
     {
-        isActive = false;
+        activeOverlay = null;
     }
 }
