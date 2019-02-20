dce601b1b8923659563430ae5bb59d934df016a1
Jesse Morgan <Jesse@Arwen.local>
2019-02-19 21:13:56 -0800

    added falling cushion

---
+++ Assets/Scripts/FallingCushion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingCushion : MonoBehaviour
+{
+
+    [SerializeField] float cushionDropTime = 1f;
+    [SerializeField] float cushionDestroyTime = 3f;
+ 
+    public IEnumerator DropCushion()
+    {
+        yield return new WaitForSeconds(cushionDropTime);
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    public IEnumerator DestroyCushion()
+    {
+        yield return new WaitForSeconds(cushionDestroyTime);
+        Destroy(gameObject);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D player)
+    {
+        StartCoroutine(DropCushion());
+        StartCoroutine(DestroyCushion());
+    }
+}
