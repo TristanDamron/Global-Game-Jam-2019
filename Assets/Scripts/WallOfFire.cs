edab525e535cebfda78c7275dd044c39a3165bce
Jesse Morgan <Jesse@Arwen.local>
2019-02-21 22:26:54 -0800

    all levels playable - game playable beginning to end

---
+++ Assets/Scripts/WallOfFire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallOfFire : MonoBehaviour
+{
+
+    [SerializeField] [Range(0.01f, 0.1f)] float fireWallSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2 (transform.position.x + fireWallSpeed, transform.position.y);
+    }
+}
